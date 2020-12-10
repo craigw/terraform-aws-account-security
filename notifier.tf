7028a456efbae039e8e5bcf92c00379ca8021e98
Craig Webster <craig@barkingiguana.com>
2020-12-10 15:16:17 +0800

    SSE for SNS

--- notifier.tf
+++ notifier.tf
@@ -3,7 +3,9 @@ module "notify_slack" {
 
   role_arn = "${var.role_arn}"
 
-  sns_topic_name    = "security-alerts"
+  sns_topic_name       = "security-alerts"
+  sns_topic_kms_key_id = "alias/aws/sns"
+
   slack_webhook_url = "${var.slack_notifier_webhook_url}"
   slack_channel     = "${var.slack_notifier_channel}"
   slack_username    = "${var.slack_notifier_username}"
